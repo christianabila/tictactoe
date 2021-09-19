69d7a95f0a3b1c4466c980dd2c5442993f71a913
Christian Abila <christian@abila.at>
2021-09-19 14:12:19 +0200

    create index.php and fill it with some meta data

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+
+        <meta property="og:title" content="Tic Tac Toe" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="" />
+
+        <title>Tic Tac Toe</title>
+        <script src="assets/js/vue.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
